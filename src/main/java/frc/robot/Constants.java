64f35b5439d74b9ee58f811438801e625f4dbbdc
RileyWildbots3865 <47373241+RileyWildbots3865@users.noreply.github.com>
2025-01-27 18:27:15 -0500

    Added Blinkin LEDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,6 +6,9 @@ public final class Constants {
     public static final int DriverOne = 0;
     public static final int DriverTwo = 1;
   }
+  public static class Extras {
+    public static final int blinkinID = 1;
+  }
   public static final class DriveConstants {
     public static final double kMaxSpeedMetersPerSecond = 6.3;
     public static final double kMaxAngularSpeed = 3.8 * Math.PI; // radians per second
---
+++ src/main/java/frc/robot/subsystems/subBlinkin.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+import frc.robot.Constants.Extras;
+
+public class subBlinkin extends SubsystemBase {
+  /** Creates a new subBlinkin. */
+  private final Spark blinkin;
+
+  public subBlinkin() {
+    blinkin = new Spark(Extras.blinkinID);
+  }
+
+  public void confetti() {
+    blinkin.set(-0.87);
+  }
+
+  public void red() {
+    blinkin.set(0.61 );
+  }
+
+  public void blue() {
+    blinkin.set(0.87);
+  }
+
+  public void random() {
+    double randDouble = 2*Math.floor(100*Math.random()) - 99;
+    blinkin.set(randDouble / 100);
+  }
+  
+  public void rainbow() {
+    blinkin.set(-.99);
+  }
+
+  public void riley() {
+    blinkin.set(0.51 );
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
