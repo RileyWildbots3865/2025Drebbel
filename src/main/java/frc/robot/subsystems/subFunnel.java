acfa81b5e6278b8186b0047d6d52c7fdb14ed124
RileyWildbots3865 <47373241+RileyWildbots3865@users.noreply.github.com>
2025-01-30 17:31:46 -0500

    Added Funnel Motor

--- src/main/java/frc/robot/subsystems/subFunnel.java
+++ src/main/java/frc/robot/subsystems/subFunnel.java
@@ -5,10 +5,21 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.MechConstants;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class subFunnel extends SubsystemBase {
   /** Creates a new subFunnel. */
-  public subFunnel() {}
+  SparkMax funnelMotor;
+  
+  public subFunnel() {
+    funnelMotor = new SparkMax(MechConstants.funnelCanId, MotorType.kBrushless);
+    funnelMotor.configure(null, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
 
   @Override
   public void periodic() {
