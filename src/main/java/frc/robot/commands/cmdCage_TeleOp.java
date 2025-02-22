b134e45bd6fcca9dadce5ec003203f4133f65154
RileyWildbots3865 <47373241+RileyWildbots3865@users.noreply.github.com>
2025-02-22 17:06:19 -0500

    Added Motor For Cage

---
+++ src/main/java/frc/robot/commands/cmdCage_TeleOp.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.subCage;
+
+
+public class cmdCage_TeleOp extends Command {
+    private subCage cage;
+    private boolean up;
+
+    public cmdCage_TeleOp(subCage cage, boolean up) {
+        this.cage = cage;
+        this.up = up;
+    }
+
+    @Override
+    public void execute() {
+      cage.cageMotor.set((up) ? Constants.Cage.cageSpeed : -Constants.Cage.cageSpeed);
+    }
+
+    @Override
+    public void end(boolean isFinished) {
+      cage.cageMotor.set(0);
+    }
+}
