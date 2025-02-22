f257b06dda07667a2ecbb00558ffaa87a1d9017d
RileyWildbots3865 <47373241+RileyWildbots3865@users.noreply.github.com>
2025-02-22 17:06:50 -0500

    Fixed

--- src/main/java/frc/robot/subsystems/subIntake.java
+++ src/main/java/frc/robot/subsystems/subIntake.java
@@ -14,21 +14,26 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class subIntake extends SubsystemBase {
-  /** Creates a new subIntake. */
+  // Creates a new subIntake. 
   public SparkMax intakeMotor1;
   public SparkMax intakeMotor2;
 
   SparkMaxConfig intakeConfig = new SparkMaxConfig();
+  SparkMaxConfig intakeConfig2 = new SparkMaxConfig();
 
   public subIntake() {
-    intakeConfig.follow(intakeMotor1, true);
-
     intakeMotor1 = new SparkMax(Intake.intake1CanId, MotorType.kBrushless);
-    intakeMotor1.configure(null, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     intakeMotor2 = new SparkMax(Intake.intake2CanId, MotorType.kBrushless);
-    intakeMotor2.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    intakeConfig.idleMode(IdleMode.kCoast);
+    intakeConfig2.idleMode(IdleMode.kCoast);
+    intakeConfig2.follow(intakeMotor1, true);
+
+    intakeMotor1.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    intakeMotor2.configure(intakeConfig2, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   @Override
@@ -36,3 +41,4 @@ public class subIntake extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 }
+
